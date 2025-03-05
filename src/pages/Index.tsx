a51b23b0c6c64c3d1056777e5ebd968007746d1c
Aliaksandr <100074523+thisopen@users.noreply.github.com>
2025-03-05 15:04:49 +0100

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -63,7 +63,7 @@ const Index = () => {
       {/* Catalog Section */}
         <section className="relative z-10 bg-black/0 py-2">
           <div className="container mx-auto px-0">   
-            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-6 gap-1">
+            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-6 gap-0">
               {/* Кольца */}
               <a href="#rings" className="flex flex-col items-center text-center text-white hover:text-gray-300 transition-colors relative">
                 <div
